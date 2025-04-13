ccbf6bda7aeaaded52b05d9e57581a05547c9cfa
Tanmaye <tanmaye@Tanmayes-Air.lan>
2025-04-13 19:08:17 -0400

    added main.py the function for railways ot call

--- main.py
+++ main.py
@@ -277,3 +277,7 @@ async def process_profile(linkedin_url: str):
         process_single_profile(linkedin_url),
         media_type='text/event-stream'
     )
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
